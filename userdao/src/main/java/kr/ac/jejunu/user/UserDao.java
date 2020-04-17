a73bcdb00176204bf01a0b1fc1222370a868d293
Renoris <55519101+Renoris@users.noreply.github.com>
2020-04-17 20:16:34 +0900

    4-17-2 실습

--- userdao/src/main/java/kr/ac/jejunu/user/UserDao.java
+++ userdao/src/main/java/kr/ac/jejunu/user/UserDao.java
@@ -1,7 +1,7 @@
 package kr.ac.jejunu.user;
 import java.sql.*;
 
-public class UserDao {
+public abstract class UserDao {
     public User get(Integer id) throws ClassNotFoundException, SQLException {
         //mysql
         Connection connection = getConnection();
@@ -27,14 +27,6 @@ public class UserDao {
 
     }
 
-    private Connection getConnection() throws ClassNotFoundException, SQLException {
-        //driver 로딩
-        Class.forName("com.mysql.cj.jdbc.Driver");
-        //connection
-        //query
-        return DriverManager.getConnection("jdbc:mysql://localhost/jeju?serverTimezone=Asia/Seoul", "jeju", "jejupw");
-    }
-
     public void insert(User user) throws ClassNotFoundException, SQLException {
         //mysql
         //driver 로딩
@@ -54,4 +46,13 @@ public class UserDao {
         connection.close();
 
     }
+
+    abstract public Connection getConnection() throws ClassNotFoundException, SQLException ;//추상화 기법-특정 메소드에 뭐가 들어갈지 모를때 씀-abstract 메소드
+       /* //driver 로딩
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        //connection
+
+        return DriverManager.getConnection("jdbc:mysql://localhost/jeju?serverTimezone=Asia/Seoul", "jeju", "jejupw");*/
+        //추상화과정-abstract로 만들고 각각 따로
+
 }
